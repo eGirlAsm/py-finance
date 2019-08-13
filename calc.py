c6cc559837b8e30b32208a2c6d1689ddff45cea7
egirlasm <egirlasm@gmail.com>
2019-08-13 15:26:47 +0900

    use code tag

--- calc.py
+++ calc.py
@@ -10,6 +10,6 @@ for x in range(1,100):
 	profit = temp
 	temp = temp +  temp * 10 / 100
 #	print('profit is ' + str(temp - profit))
-	print('in ' + str(x) + ' times the total = ' + str(locale.currency(temp,grouping=True)) + '원  이자 10% 순수익 ' + str(locale.currency(temp-profit,grouping=True)) + ' \r\n')
+	print('```in ' + str(x) + ' times the total = ' + str(locale.currency(temp,grouping=True)) + '원  이자 10% 순수익 ' + str(locale.currency(temp-profit,grouping=True)) + ' ```')
 #	print(temp)
 
